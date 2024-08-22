916e76b5f1f61425df2722494f0bcc61baeaad29
zc L <134561180+Lzc2003@users.noreply.github.com>
2024-08-22 10:28:59 +0800

    Update mapping.launch.py

--- launch/mapping.launch.py
+++ launch/mapping.launch.py
@@ -4,21 +4,20 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import LaunchConfiguration
 from launch.conditions import IfCondition
 
-from launch_ros.actions import Node
+from launch_ros.actions import Node, SetUseSimTime
 
 
 def generate_launch_description():
     package_path = get_package_share_directory('fast_lio')
-    default_config_path = os.path.join(package_path, 'config')
+    default_config_path = os.path.join(package_path, 'config', 'mid360.yaml')
     default_rviz_config_path = os.path.join(
         package_path, 'rviz', 'fastlio.rviz')
 
     use_sim_time = LaunchConfiguration('use_sim_time')
     config_path = LaunchConfiguration('config_path')
-    config_file = LaunchConfiguration('config_file')
     rviz_use = LaunchConfiguration('rviz')
     rviz_cfg = LaunchConfiguration('rviz_cfg')
 
@@ -29,10 +28,6 @@ def generate_launch_description():
     declare_config_path_cmd = DeclareLaunchArgument(
         'config_path', default_value=default_config_path,
         description='Yaml config file path'
-    )
-    decalre_config_file_cmd = DeclareLaunchArgument(
-        'config_file', default_value='mid360.yaml',
-        description='Config file'
     )
     declare_rviz_cmd = DeclareLaunchArgument(
         'rviz', default_value='true',
@@ -46,7 +41,7 @@ def generate_launch_description():
     fast_lio_node = Node(
         package='fast_lio',
         executable='fastlio_mapping',
-        parameters=[PathJoinSubstitution([config_path, config_file]),
+        parameters=[config_path,
                     {'use_sim_time': use_sim_time}],
         output='screen'
     )
@@ -56,15 +51,21 @@ def generate_launch_description():
         arguments=['-d', rviz_cfg],
         condition=IfCondition(rviz_use)
     )
+    # lidar_tf = Node(
+    #     name='lidar_tf',
+    #     package='tf2_ros',
+    #     executable='static_transform_publisher',
+    #     arguments=['0','0','-0.55','0','0','0','1','body','base_link']
+    #     )
 
     ld = LaunchDescription()
     ld.add_action(declare_use_sim_time_cmd)
     ld.add_action(declare_config_path_cmd)
-    ld.add_action(decalre_config_file_cmd)
     ld.add_action(declare_rviz_cmd)
     ld.add_action(declare_rviz_config_path_cmd)
 
     ld.add_action(fast_lio_node)
-    ld.add_action(rviz_node)
+    # ld.add_action(lidar_tf)
+    # ld.add_action(rviz_node)
 
     return ld
